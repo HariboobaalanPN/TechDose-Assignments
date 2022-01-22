fa1ffd965ca3a8c90f1138724d426d5deccceb63
HariboobaalanPN <79743324+HariboobaalanPN@users.noreply.github.com>
2022-01-22 18:52:41 +0530

    Update Bitwise AND of Range.c

--- Bit Manipulation/Bitwise AND of Range.c
+++ Bit Manipulation/Bitwise AND of Range.c
@@ -1,13 +1,13 @@
-#include<stdio.h
+#include<stdio.h>
 
-int bitwiseAndofRange(int a, int b){
-	while (a != b) {
-        b &= (b - 1);
-        while (a > b) {
-            a &= (a - 1);
-        }
-    }
-    return b;
+int bitwiseAndofRange(int left, int right){
+	int count=0;
+	while (left!=right){
+		left>>=1;
+		right>>=1;
+		count++;
+	}
+	return left<<count;
 }
 
 
@@ -15,6 +15,6 @@ int main(){
 	 int st,end;
 	 printf("Enter start and end range:");
 	 scanf("%d %d",&st,&end);
-	 printf("Bitwise AND from %d to %d  is %d",st,end,bitwiseAndofRange(st,end));
+	 printf("\nBitwise AND from %d to %d  is %d",st,end,bitwiseAndofRange(st,end));
 	
 }
