c31eebbc31c2a1ceb97cd46f639671d2d0d18f63
HariboobaalanPN <79743324+HariboobaalanPN@users.noreply.github.com>
2022-01-22 18:16:01 +0530

    Update Majority element.c

--- Bit Manipulation/Majority element.c
+++ Bit Manipulation/Majority element.c
@@ -4,18 +4,22 @@
 int majorityElement(int n,int arr[]){
     int major=arr[0],count=0;
     for (int i=1;i<n;i++){
-		count=(major==arr[i])?count+1:count-1;
+    	count=(major==arr[i])?count+1:count-1;
+    	if(count==0){
+    	    major=arr[i];
+    	    count=1;
+    	}
     }
     return major;
 } 
 
 int main(){
-	 int n;
-	 printf("Enter number of elements:");
-	 scanf("%d\n",&n);
-	 int arr[n];
-	 printf("\nEnter elements: ");
-	 for(int i=0;i<n;i++)
-	    scanf("%d ",&arr[i]);
-	 printf("\nElement is %d.",majorityElement(n,arr));
+    int n;
+    printf("Enter number of elements:");
+    scanf("%d\n",&n);
+    int arr[n];
+    printf("\nEnter elements: ");
+    for(int i=0;i<n;i++)
+    scanf("%d ",&arr[i]);
+    printf("\nElement is %d.",majorityElement(n,arr));
 }
