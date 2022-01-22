bec38b0c38fad4f57162feab8334bd58b4120420
HariboobaalanPN <79743324+HariboobaalanPN@users.noreply.github.com>
2022-01-22 23:47:04 +0530

    Update Kth largest element.c

--- Array/Kth largest element.c
+++ Array/Kth largest element.c
@@ -1,16 +1,32 @@
 #include<stdio.h>
 #include<string.h>
-int kThlargest(int n,int arr[n],int k) {
-    for(int i=0;i<n-1;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[i]>arr[j]){
-                int t=arr[i];
-                arr[i]=arr[j];
-                arr[j]=t;
-            }
+int partition(int arr[],int low, int high){
+    int pivot=arr[high];
+    int i=low-1;
+    for(int j=low;j<high;j++){
+        if(arr[j]<pivot){
+            i++;
+            int t=arr[i];
+            arr[i]=arr[j];
+            arr[j]=t;
         }
     }
-    return arr[n-k];
+    int t=arr[i+1];
+    arr[i+1]=arr[high];
+    arr[high]=t;
+    return i+1;
+}
+void quickSort(int arr[], int low, int high){
+    if(low<high){
+        int pi = partition(arr,low,high);
+        quickSort(arr,low,pi-1);
+        quickSort(arr,pi+1,high);
+    }
+}
+
+int findKthLargest(int* nums, int numsSize, int k){
+    quickSort(nums,0,numsSize-1);
+    return nums[numsSize-k];
 }
 
 
@@ -23,6 +39,5 @@ int main(){
 	 for(int i=0;i<n;i++)
 	    scanf("%d ",&arr[i]);
 	 scanf("%d",&k);
-	 printf("\n%d",kThlargest(n,arr,k));
-	
+	 printf("\n%d",findKthLargest(arr,n,k));
 }
