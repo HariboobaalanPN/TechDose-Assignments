2ed83d2b85627ac5c44be0daef23b624666544f3
HariboobaalanPN <79743324+HariboobaalanPN@users.noreply.github.com>
2022-01-22 19:10:21 +0530

    Update Counting bits.c

--- Bit Manipulation/Counting bits.c
+++ Bit Manipulation/Counting bits.c
@@ -1,12 +1,14 @@
 #include<stdio.h>
 
 void countBits(int n){
-    n++;
-    int arr[n];
+    int arr[n+1];
     arr[0]=0;
-    for(int i=1;i<n;i++)
-        arr[i]=(i&1)?arr[i>>1]+1:arr[i>>1];
-    for(int i=0;i<n;i++){
+    
+    for(int i=1;i<=n;i++){
+        arr[i]=arr[n/2]+i%2;
+    }
+    
+    for(int i=0;i<=n;i++){
         printf("%d ",arr[i]);
     }
 } 
@@ -17,6 +19,7 @@ int main(){
 	 int n;
 	 printf("Enter number:");
 	 scanf("%d",&n);
+	 printf("\nCountin Bits:   ");
 	 countBits(n);
 	
 }
